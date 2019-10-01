71e4e8273d8da8792dbfced95ada40603d597ef8
mateusz.witon <mateusz.witon@globallogic.com>
2019-10-01 13:20:56 +0200

    Added handling users from Google

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shoppage/shoppage.component'
 import Header from "./components/header/header.component";
 import Sign_in_page from "./pages/sign_in_page/sign_in.component";
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component {
     constructor(promps) {
@@ -21,9 +21,9 @@ class App extends React.Component {
     unsubscribeFromAuth = () => {};
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+            await createUserProfileDocument(user);
             this.setState({currentUser: user});
-            console.log(user);
         });
     }
 
