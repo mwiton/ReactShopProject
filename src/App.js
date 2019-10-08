9d7b87dccdc116e829372dfbe0b8e9e104a0d00f
Mateusz Witoń <mwiton@gmail.com>
2019-10-08 23:08:29 +0200

    Added Redux

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser} />
+                <Header/>
                 <Switch>
                     <Route path='/' component={HomePage} exact/>
                     <Route path='/shop/' component={ShopPage} exact/>
